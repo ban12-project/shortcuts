8d8d743162316c8f7887724efc46613dd6ad0e49
onnlooker <github-action@actions-template-sync.noreply.github.com>
2024-04-01 01:04:03 +0000

    chore(template): merge template changes :up:
    Signed-off-by: onnlooker <github-action@actions-template-sync.noreply.github.com>

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,4 @@
 import type { Config } from 'tailwindcss'
-import plugin from 'tailwindcss/plugin'
 
 const config: Config = {
   content: [
@@ -7,105 +6,15 @@ const config: Config = {
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  darkMode: 'class',
   theme: {
-    container: {
-      center: true,
-      padding: '2rem',
-      screens: {
-        '2xl': '1400px',
-      },
-    },
     extend: {
-      keyframes: {
-        slideDownAndFade: {
-          from: { opacity: '0', transform: 'translateY(-2px)' },
-          to: { opacity: '1', transform: 'translateY(0)' },
-        },
-        slideLeftAndFade: {
-          from: { opacity: '0', transform: 'translateX(2px)' },
-          to: { opacity: '1', transform: 'translateX(0)' },
-        },
-        slideUpAndFade: {
-          from: { opacity: '0', transform: 'translateY(2px)' },
-          to: { opacity: '1', transform: 'translateY(0)' },
-        },
-        slideRightAndFade: {
-          from: { opacity: '0', transform: 'translateX(-2px)' },
-          to: { opacity: '1', transform: 'translateX(0)' },
-        },
-        enterFromRight: {
-          from: { opacity: '0', transform: 'translateX(200px)' },
-          to: { opacity: '1', transform: 'translateX(0)' },
-        },
-        enterFromLeft: {
-          from: { opacity: '0', transform: 'translateX(-200px)' },
-          to: { opacity: '1', transform: 'translateX(0)' },
-        },
-        exitToRight: {
-          from: { opacity: '1', transform: 'translateX(0)' },
-          to: { opacity: '0', transform: 'translateX(200px)' },
-        },
-        exitToLeft: {
-          from: { opacity: '1', transform: 'translateX(0)' },
-          to: { opacity: '0', transform: 'translateX(-200px)' },
-        },
-        scaleIn: {
-          from: { opacity: '0', transform: 'rotateX(-10deg) scale(0.9)' },
-          to: { opacity: '1', transform: 'rotateX(0deg) scale(1)' },
-        },
-        scaleOut: {
-          from: { opacity: '1', transform: 'rotateX(0deg) scale(1)' },
-          to: { opacity: '0', transform: 'rotateX(-10deg) scale(0.95)' },
-        },
-        fadeIn: {
-          from: { opacity: '0' },
-          to: { opacity: '1' },
-        },
-        fadeOut: {
-          from: { opacity: '1' },
-          to: { opacity: '0' },
-        },
-        'accordion-down': {
-          from: { height: '0' },
-          to: { height: 'var(--radix-accordion-content-height)' },
-        },
-        'accordion-up': {
-          from: { height: 'var(--radix-accordion-content-height)' },
-          to: { height: '0' },
-        },
-      },
-      animation: {
-        slideDownAndFade:
-          'slideDownAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)',
-        slideLeftAndFade:
-          'slideLeftAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)',
-        slideUpAndFade: 'slideUpAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)',
-        slideRightAndFade:
-          'slideRightAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)',
-        scaleIn: 'scaleIn 200ms ease',
-        scaleOut: 'scaleOut 200ms ease',
-        fadeIn: 'fadeIn 200ms ease',
-        fadeOut: 'fadeOut 200ms ease',
-        enterFromLeft: 'enterFromLeft 250ms ease',
-        enterFromRight: 'enterFromRight 250ms ease',
-        exitToLeft: 'exitToLeft 250ms ease',
-        exitToRight: 'exitToRight 250ms ease',
-        'accordion-down': 'accordion-down 0.2s ease-out',
-        'accordion-up': 'accordion-up 0.2s ease-out',
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
   },
-  plugins: [
-    require('tailwindcss-animate'),
-    require('@ban12/tailwindcss-safe-area'),
-    plugin(({ matchUtilities }) => {
-      matchUtilities({
-        perspective: (value) => ({
-          perspective: value,
-        }),
-      })
-    }),
-  ],
+  plugins: [],
 }
 export default config
