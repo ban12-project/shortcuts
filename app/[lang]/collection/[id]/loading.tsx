7251d13879811e83da179c9b03f9895a204a1852
Coda <o4oban@gmail.com>
2024-04-17 15:34:41 +0800

    chore: prettier

--- app/[lang]/collection/[id]/loading.tsx
+++ app/[lang]/collection/[id]/loading.tsx
@@ -4,7 +4,7 @@ import { Skeleton } from '#/components/ui/skeleton'
 export default function Loading() {
   return (
     <>
-      <div className="container-full pt-safe-max-4 pb-5">
+      <div className="container-full pb-5 pt-safe-max-4">
         <Skeleton className="h-9 w-full" />
       </div>
       <div>
